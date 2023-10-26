1bee9ff778d975059a1bccb674750576bf7a5e33
Mateusz Szybicki <mateusz.szybicki@interia.pl>
2023-10-26 19:20:37 +0200

    add getter functions to User class

--- library/user.cc
+++ library/user.cc
@@ -1,7 +1,18 @@
 #include "user.hh"
 
-User::User(std::string first_name, std::string last_name, std::string number) 
+User::User(std::string first_name, std::string last_name, std::string number)
     : first_name{first_name}, last_name{last_name}, number{number}
 {
-    
+}
+
+std::string User::get_first_name() {
+    return first_name;
+}
+
+std::string User::get_last_name() {
+    return last_name;
+}
+
+std::string User::get_number() {
+    return number;
 }
--- library/user.hh
+++ library/user.hh
@@ -1,5 +1,5 @@
-#ifndef _USER_
-#define _USER_
+#ifndef _USER_H_
+#define _USER_H_
 
 #include <string>
 
@@ -7,9 +7,12 @@ class User {
 public:
     User(std::string first_name, std::string last_name, std::string number);
 
+    std::string get_first_name();
+    std::string get_last_name();
+    std::string get_number();
 
 
-private:
+protected:
     std::string first_name;
     std::string last_name;
     std::string number;
