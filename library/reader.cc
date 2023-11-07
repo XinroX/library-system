c3dc9ef538370f949a65c07f5ee8e45e481dcd05
Mateusz Szybicki <mateusz.szybicki@interia.pl>
2023-11-07 19:28:33 +0100

    add reader class

---
+++ library/reader.cc
@@ -0,0 +1,18 @@
+#include "reader.hh"
+
+Reader::Reader(std::string first_name, std::string last_name, std::string number)
+    : User{first_name, last_name, number}
+{
+}
+
+void Reader::add_item(std::shared_ptr<Item> item)
+{
+    if (std::find(items.begin(), items.end(), item) == items.end()) {
+        items.push_back(item);
+    }
+    else {
+        std::cout << "Item already exists\n";
+    }
+
+    std::cout << items.size() << '\n';
+}
---
+++ library/reader.hh
@@ -0,0 +1,20 @@
+#ifndef _READER_H_
+#define _READER_H_
+#include "user.hh"
+#include "item.hh"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+using Items = std::vector<std::shared_ptr<Item>>;
+
+class Reader : public User {
+public:
+    Reader(std::string first_name, std::string last_name, std::string number);
+    void add_item(std::shared_ptr<Item> item);
+private:
+Items items;
+};
+
+
+#endif
