ec4c8d608d6cda7df9584fb5aa10aa6785ef58aa
Mateusz Szybicki <mateusz.szybicki@interia.pl>
2024-04-09 01:03:56 +0200

    Add unit tests

---
+++ library/library_tests.cc
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include "item.hh"
+#include "reader.hh"
+#include "library.hh"
+
+TEST(LibraryTest, AddItemToReader) {
+    auto item = std::make_shared<Item>("Test", "Test", "Test");
+    auto reader = std::make_shared<Reader>("Test", "Test", "00001");
+
+    reader->add_item(item);
+
+    ASSERT_EQ(1, reader->get_items().size());
+}
+
+TEST(LibraryTest, Add2ItemsToReader) {
+    auto item = std::make_shared<Item>("Test", "Test", "Test");
+    auto item2 = std::make_shared<Item>("Test", "Test", "Test");
+    auto reader = std::make_shared<Reader>("Test", "Test", "00001");
+
+    reader->add_item(item);
+    reader->add_item(item2);
+
+    ASSERT_EQ(2, reader->get_items().size());
+    
+}
+
+TEST(LibraryTest, Add2SameItemsToReader) {
+    auto item = std::make_shared<Item>("Test", "Test", "Test");
+    auto reader = std::make_shared<Reader>("Test", "Test", "00001");
+
+    reader->add_item(item);
+    reader->add_item(item);
+
+    ASSERT_EQ(1, reader->get_items().size());
+
+}
+
+TEST(LibraryTest, GetItemsFromWrongReaderNumber) {
+    Library library;
+
+    ASSERT_EQ(0, library.getItemsByReaderNumber("000000").size());
+
+}
+
+TEST(LibraryTest, AddReaderWithoutItems) {
+    Library library;
+    auto reader = std::make_shared<Reader>("Test", "Test", "00001");
+
+    library.add_reader(reader);
+
+    ASSERT_EQ(0, library.getItemsByReaderNumber("00001").size());
+}
+
+int main() {
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
