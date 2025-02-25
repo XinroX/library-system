f223b42c6b859899854455a96bfcfce17b138eee
Mateusz Szybicki <mateusz.szybicki@interia.pl>
2025-02-25 20:44:20 +0100

    Integrate http server to whole project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// #include "library.hh"
+// #include <memory>
+// #include "item.hh"
+#include <chrono>
+#include <iostream>
+#include "http_server/server.hpp"
+
+
+int main() {
+    Server server("http://localhost:8080");
+    while (true)
+    {
+        
+    }
+    return 0;
+}
